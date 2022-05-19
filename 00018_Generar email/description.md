89dbedd55c5e911ee22f3e90994eca515eb43e1d
mayra@mumuki.org <mayra@mumuki.org>
2022-05-19 15:56:25 +0000

    Mumuki Export on 2022-05-19 15:56:25 +0000

--- 00018_Generar email/description.md
+++ 00018_Generar email/description.md
@@ -1,6 +1,6 @@
 > Definí una función `generarEmail` que reciba como argumentos dos string `usuario` y `dominio` y el un string email con el formato `usuario@dominio.com`
 >
 ```javascript
-ム generarEmail('adalovelace', 'gmail')
-'adalovelace@gmail.com'
+ム generarEmail('valentinaTereshkova', 'gmail')
+'valentinaTereshkova@gmail.com'
 ```
--- 00018_Generar email/test..js
+++ 00018_Generar email/test..js
@@ -1,9 +1,9 @@
-it("Si hacemos generarEmail('adalovelace', 'gmail') debería retornar 'adalovelace@gmail.com'", function() {
-  let resultado = generarEmail('adalovelace', 'gmail')
-  assert.equal(resultado, 'adalovelace@gmail.com',`generarEmail('adalovelace', 'gmail') debería retornar adalovelace@gmail.com pero fue ${resultado}`);
+it("Si hacemos generarEmail('valentinaTereshkova', 'gmail') debería retornar 'valentinaTereshkova@gmail.com'", function() {
+  let resultado = generarEmail('valentinaTereshkova', 'gmail')
+  assert.equal(resultado, 'valentinaTereshkova@gmail.com',`generarEmail('valentinaTereshkova', 'gmail') debería retornar valentinaTereshkova@gmail.com pero fue ${resultado}`);
 })
 
-it("Si hacemos generarEmail('alfonsinaStorni', 'gmail') debería retornar 'alfonsinaStorni@gmail.com'", function() {
-  let resultado = generarEmail('alfonsinaStorni', 'gmail')
-  assert.equal(resultado, 'alfonsinaStorni@gmail.com',`generarEmail('alfonsinaStorni', 'gmail') debería retornar alfonsinaStorni@gmail.com pero fue ${resultado}`);
+it("Si hacemos generarEmail('steveWozniak', 'gmail') debería retornar 'steveWozniak@gmail.com'", function() {
+  let resultado = generarEmail('steveWozniak', 'gmail')
+  assert.equal(resultado, 'steveWozniak@gmail.com',`generarEmail('steveWozniak', 'gmail') debería retornar steveWozniak@gmail.com pero fue ${resultado}`);
 })
