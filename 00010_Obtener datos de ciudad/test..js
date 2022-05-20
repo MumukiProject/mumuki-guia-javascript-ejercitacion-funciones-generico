49026969982ae656d9b04adb006335014b20f88c
mayra@mumuki.org <mayra@mumuki.org>
2022-05-20 18:57:07 +0000

    Mumuki Export on 2022-05-20 18:57:07 +0000

--- 00010_Obtener datos de ciudad/test..js
+++ 00010_Obtener datos de ciudad/test..js
@@ -1,4 +1,4 @@
 it("Si hacemos obtenerDatosDeCiudad('Lima', 9674755, 'Perú') debería retornar La ciudad de Lima tiene una población de 9674755 habitantes y está ubicada en Perú", function() {
 let resultado = obtenerDatosDeCiudad('Lima', 9674755, 'Perú')
-assert.equal(resultado, 'La ciudad de Lima tiene una población de 545606 habitantes y está ubicada en Perú',`obtenerDatosDeCiudad('Lima', 545606, 'Perú') debería retornar La ciudad de Lima tiene una población de 545606 habitantes y está ubicada en Perú pero el resultado fue ${resultado}`);
+assert.equal(resultado, 'La ciudad de Lima tiene una población de 9674755 habitantes y está ubicada en Perú',`obtenerDatosDeCiudad('Lima', 9674755, 'Perú') debería retornar La ciudad de Lima tiene una población de 9674755 habitantes y está ubicada en Perú pero el resultado fue ${resultado}`);
 })
